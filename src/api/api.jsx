508ffa02c5ae969904761dc511ac5caf63348b2f
aydansn <aydansn@code.edu.az>
2024-09-26 21:34:48 +0400

    add env file

--- src/api/api.jsx
+++ src/api/api.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-const API_BASE = "http://localhost:3000";
-
+const API_BASE = import.meta.env.VITE_API_BASE_URL;
+console.log(API_BASE);
 export const getStatusList = () => axios.get(`${API_BASE}/status/getList`);
 
 export const getTodoList = () => axios.get(`${API_BASE}/todo/getList`);
